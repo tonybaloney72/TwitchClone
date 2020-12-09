fa13c506e705329910922f517d1cb8572c01d575
Anthony Bologna <bologna.anthony@gmail.com>
2020-12-09 11:50:11 -0800

    basic seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.delete_all
+
+user1 = User.create({email: 'demouser@email.com', username: 'SwitchDemoUser', password: '123456'})
+user2 = User.create({email: 'baloney@email.com', username: 'TonyBaloney', password: '654321'})
+user3 = User.create({email: 'thirduser@email.com', username: 'UserNumbahThree', password: 'NumbahThr33'})
