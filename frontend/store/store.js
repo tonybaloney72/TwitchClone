dba88ddc4e21d4602a7976dd94c8239859405b68
Anthony Bologna <bologna.anthony@gmail.com>
2020-12-10 16:55:19 -0800

    Adding in conditional to not push logger to Heroku

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -3,8 +3,13 @@ import RootReducer from '../reducers/root_reducer';
 import thunk from 'redux-thunk'
 import logger from 'redux-logger'
 
-const configureStore = (preloadedState = {}) => (
-    createStore(RootReducer, preloadedState, applyMiddleware(thunk, logger))
-)
+const configureStore = (preloadedState = {}) => {
+
+    let middleware = [thunk];
+    if (process.env.NODE_ENV !== 'production') {
+        middleware = [...middleware, logger];
+    }
+    return createStore(RootReducer, preloadedState, applyMiddleware(...middleware))
+};
 
 export default configureStore;
