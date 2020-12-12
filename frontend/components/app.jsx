de0bbb33f069d75f3ddbf31400ebf5d1554722a3
Anthony Bologna <bologna.anthony@gmail.com>
2020-12-11 22:06:23 -0800

    minor styling fixes

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,11 +3,10 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import { AuthRoute } from '../utils/route_util'
 
 import Modal from './modal/modal';
-import SignupContainer from './session/signup_container'
-import LoginContainer from './session/login_container'
 import SplashContainer from './splash/splash_container'
 import NavBarContainer from './navbar/navbar_container'
-import clip_container from './clips/clip_show_container';
+import ClipShowContainer from './clips/clip_show_container';
+import ClipFormContainer from './clips/clip_form_container';
 
 const App = () => {
     return (
@@ -15,7 +14,8 @@ const App = () => {
             <Modal />
             <NavBarContainer />
             <Switch>
-                <Route exact path="/clips/:clipId" component={clip_container} />
+                <Route exact path="/upload" component={ClipFormContainer}/> //make AuthRoute
+                <Route exact path="/clips/:clipId" component={ClipShowContainer} />
                 <Route exact path="/" component={SplashContainer}/>
                 <Redirect to="/" />
             </Switch>
